9e2751ca2a696683ec458283c34687e91bbf94d0
ClaudCZX <claudio.delafuente@opencasa.com>
2022-09-08 22:04:33 -0400

    add tables for qa cuestions

--- Aplicaciones/Clientes/admin.py
+++ Aplicaciones/Clientes/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Clientes
+from .models import TipoCalidad
+from .models import PreguntasCalidad
+
 # Register your models here.
 
 admin.site.register(Clientes)
+admin.site.register(TipoCalidad)
+admin.site.register(PreguntasCalidad)
